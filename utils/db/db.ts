fbc6c26df748f1191554536448f6c4a835d98c68
P <pmmoralesj@gmail.com>
2021-02-06 00:46:45 -0500

    Change env variable names

--- utils/db/db.ts
+++ utils/db/db.ts
@@ -3,16 +3,16 @@ import 'firebase/firestore'
 
 function getCredentials() {
     return {
-        type: process.env.TYPE,
-        projectId: process.env.PROJECT_ID,
-        privateKeyId: process.env.PRIVATE_KEY_ID,
-        privateKey: process.env.PRIVATE_KEY,
-        clientEmail: process.env.CLIENT_EMAIL,
-        clientId: process.env.CLIENT_ID,
-        authUri: process.env.AUTH_URI,
-        tokenUri: process.env.TOKEN_URI,
-        authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
-        clientX509CertUrl: process.env.CLIENT_X509_CERT_URL
+        type: process.env.FIREBASE_TYPE,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        clientId: process.env.FIREBASE_CLIENT_ID,
+        authUri: process.env.FIREBASE_AUTH_URI,
+        tokenUri: process.env.FIREBASE_TOKEN_URI,
+        authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL
     }
 }
 
