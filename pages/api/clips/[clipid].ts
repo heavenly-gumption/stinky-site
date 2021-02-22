cbd619c527d4bb45b5646588cae26cf4fffa0b0f
Chudooder <codeshawnachu@gmail.com>
2021-02-21 16:33:01 -0800

    Clips are now loaded in chunks

--- pages/api/clips/[clipid].ts
+++ pages/api/clips/[clipid].ts
@@ -1,23 +1,6 @@
-import S3 from "aws-sdk/clients/s3"
-
 import { Clip } from "../../../types/models"
 import { ClipModel } from "../../../utils/db/models/clips"
 import { Database } from "../../../utils/db/db"
-
-const s3 = new S3({
-  accessKeyId: process.env.SITE_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.SITE_AWS_SECRET
-})
-
-async function getClipDataFromS3(clip: Clip): Promise<Buffer> {
-  const params = {
-    Bucket: process.env.SITE_AWS_S3_BUCKET,
-    Key: `clips/${clip.id}.pcm`
-  }
-
-  const data = await s3.getObject(params).promise()
-  return data.Body as Buffer
-}
 
 export default async (req, res) => {
   const {
@@ -25,11 +8,7 @@ export default async (req, res) => {
   } = req
   try {
     const clip = await ClipModel(Database()).getClip(clipid)
-    const clipData = await getClipDataFromS3(clip)
-    res.status(200).json({
-      ...clip,
-      data: clipData.toString('base64')
-    })
+    res.status(200).json(clip)
   } catch (err) {
     console.error(err)
     res.status(500).json('Unexpected error when getting clip')
