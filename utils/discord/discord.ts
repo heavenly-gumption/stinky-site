72e414ab7673a5b3a15469d45926c35cc925a1ca
Chudooder <codeshawnachu@gmail.com>
2021-12-11 15:19:56 -0800

    Fix build

--- utils/discord/discord.ts
+++ utils/discord/discord.ts
@@ -5,7 +5,7 @@ var client: Client
 export function DiscordClient() {
   if (client) return client;
 
-  client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+  client = new Client()
   client.login(process.env.DISCORD_TOKEN)
   return client
 }
